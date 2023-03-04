f6943e6f1723f68e029a195a247e3b8f36cee24a
m-slekys <slekysmantas1@gmail.com>
2023-03-04 17:14:03 +0200

    add iron man

---
+++ bass1.rb
@@ -0,0 +1,39 @@
+while (TRUE)
+  
+  play 45 ,release: 0.6
+  sleep 1
+  
+  play 48,release: 0.6
+  sleep 1
+  
+  play 48,release: 0.6
+  sleep 0.5
+  
+  play 50,release: 0.6
+  sleep 0.5
+  play 50,release: 0.6
+  sleep 1
+  
+  play 53,release: 0.6
+  sleep 0.25
+  play 52,release: 0.6
+  sleep 0.25
+  play 53,release: 0.6
+  sleep 0.25
+  play 52,release: 0.6
+  sleep 0.25
+  play 53,release: 0.6
+  sleep 0.25
+  play 52,release: 0.6
+  sleep 0.25
+  
+  play 48,release: 0.6
+  sleep 0.5
+  play 48,release: 0.6
+  sleep 0.5
+  
+  play 50,release: 0.6
+  sleep 0.5
+  play 50,release: 0.6
+  sleep 1
+end
